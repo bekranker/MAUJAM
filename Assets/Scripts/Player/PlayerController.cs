129a1417ab9d01673f7a0b32cf3ac64a800c095d
Bekir <bekirekiz@ogrenci.beykoz.edu.tr>
2024-02-23 18:54:16 +0300

    firstInitial

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _rb;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _jumpForce;
+
+
+}
