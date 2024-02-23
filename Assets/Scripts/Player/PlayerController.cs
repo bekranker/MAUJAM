ca8753c5fcb782efb054e37b88a9c33915603765
Mert Ömer Aslan <mertomeraslan@ogrenci.beykoz.edu.tr>
2024-02-23 22:47:44 +0300

    contolr
    control

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -17,13 +17,26 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float lastJumpTime;
     [SerializeField] private bool isJumping;
     [SerializeField] private bool jumpInputReleased;
-    public bool Grounded;
+    private Grounded _grounded;
+
+    void Start()
+    {
+        _grounded = GetComponent<Grounded>();
+    }
     private void Update()
     {
         _moveInput = Input.GetAxis("Horizontal");
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            jump();
+        }
     }
     private void jump()
     {
+        if (!_grounded.IsGrounded())
+            return;
+
+        
         _rb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
         lastGroundedTime = 0;
         lastJumpTime = 0;
